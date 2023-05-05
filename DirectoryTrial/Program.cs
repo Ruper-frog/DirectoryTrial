ffaaf64f31ff5844d206b48a04dedfc9518462e0
Ruper Ottolenghi <ruper120406@gmail.com>
2023-05-05 11:38:00 +0300

    Was able to extract the year of the photo

--- DirectoryTrial/Program.cs
+++ DirectoryTrial/Program.cs
@@ -8,28 +8,45 @@ class Program
 {
     static void Main(string[] args)
     {
-        if (!File.Exists(path))
-        {
-            // This statement ensures that the file is created,
-            // but the handle is not kept.
-            using (FileStream fs = File.Create(path)) { }
-        }
-
-        string sourceDirectory = @"C:\MyPhotos";
+        /*string sourceDirectoryQ = @"C:\MyPhotos";
         string newDirectoryName = "Sorted";
         string newDirectoryPath = Path.Combine(sourceDirectory, newDirectoryName);
 
         // Create the new directory
-        Directory.CreateDirectory(newDirectoryPath);
+        Directory.CreateDirectory(newDirectoryPath);*/
 
-        string sourceDirectory = @"C:\Users\ruper\OneDrive\שולחן העבודה\Ruper";
-        string destinationDirectory = @"C:\Users\ruper\OneDrive\שולחן העבודה\New folder";
+        string sourceDirectory = @"C:\Users\ruper\OneDrive\שולחן העבודה\New folder";
+        string destinationDirectory = @"C:\Users\ruper\OneDrive\שולחן העבודה\Ruper";
 
         // Get all files in the source directory
         List<string> files = new List<string> (Directory.GetFiles(sourceDirectory));
 
+        List<string> filesNames = new List<string>();
+
+        foreach (string file in files)
+        {
+            int index = file.IndexOf("1");
+
+            int DotIndex = file.LastIndexOf(".");
+
+            if (index > 0)
+                filesNames.Add(file.Substring(index, file.Length - index - 1 - (file.Length - DotIndex - 1)));
+        }
+
+        /*for (int i = 0; i < filesNames.Count; i++)
+        {
+            Console.WriteLine(files[i]);
+
+            Console.WriteLine(filesNames[i]);
+        }*/
+
+        if (!File.Exists(sourceDirectory))
+        {
+
+        }
         files.Reverse();
 
+        /*
         // Iterate through each file and move it to the new directory
         foreach (string file in files)
         {
@@ -42,6 +59,6 @@ class Program
 
             // Move the file to the new location
             File.Move(file, newFilePath);
-        }
+        }*/
     }
 }
