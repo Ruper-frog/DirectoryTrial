8d418282657fef18dfe0f3c2fbcd97d34c2b9f1e
Ruper Ottolenghi <ruper120406@gmail.com>
2023-05-06 22:21:35 +0300

    Done

--- DirectoryTrial/Program.cs
+++ DirectoryTrial/Program.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
-using System.Security.Permissions;
 using System.Text.RegularExpressions;
 
 class Program
@@ -13,46 +12,8 @@ class Program
 
         string sourceDirectory = Console.ReadLine();
 
-        // Get all files in the source directory
-        List<string> files = new List<string>(Directory.GetFiles(sourceDirectory));
+        TheDeed(sourceDirectory);
 
-        foreach (string file in files)
-        {
-            int YearIndex = 0;
-
-            string Year = "";
-
-            while (true)
-            {
-                YearIndex = file.IndexOf("1", YearIndex + 1);
-
-                if (!(YearIndex != -1 && file.Length - YearIndex >= 0))
-                    goto REPEAT;
-
-                Year = file.Substring(YearIndex, 4);
-
-                if (Regex.IsMatch(Year, @"^\d+$"))
-                    break;
-            }
-            string newDirectoryName = Year;
-            string newDirectoryPath = Path.Combine(sourceDirectory, newDirectoryName);
-
-            if (!Directory.Exists(newDirectoryPath))
-            {
-                // Create the new directory
-                Directory.CreateDirectory(newDirectoryPath);
-            }
-
-            // Determine the new file name and location based on the original file name
-            string fileName = Path.GetFileName(file);
-            string newFilePath = Path.Combine(newDirectoryPath, fileName);
-
-            // Move the file to the new location
-            File.Move(file, newFilePath);
-
-            REPEAT:
-                continue;
-        }
         Console.WriteLine();
 
         Console.WriteLine("Do You Want To Open The Folder?\n\nPress Enter If You Do\n\nTo Leave Press Any Other Button");
@@ -74,5 +35,54 @@ class Program
         Console.WriteLine();
 
         Console.ReadKey();
+    }
+    static void TheDeed(string sourceDirectory)
+    {
+        // Get all files in the source directory
+        List<string> files = new List<string>(Directory.GetFiles(sourceDirectory));
+
+        List<string> folders = new List<string>(Directory.GetDirectories(sourceDirectory));
+
+        foreach (string folder in folders)
+            TheDeed(folder);
+
+        foreach (string file in files)
+        {
+            int YearIndex = 0;
+
+            string Year = "";
+
+            while (true)
+            {
+                YearIndex = file.IndexOf("1", YearIndex + 1);
+
+                if (!(YearIndex != -1 && file.Length - YearIndex >= 0))
+                    goto REPEAT;
+
+                Year = file.Substring(YearIndex, 4);
+
+                if (Regex.IsMatch(Year, @"^\d+$"))
+                    break;
+            }
+
+            string newDirectoryName = Year;
+            string newDirectoryPath = Path.Combine(sourceDirectory, newDirectoryName);
+
+            if (!Directory.Exists(newDirectoryPath))
+            {
+                // Create the new directory
+                Directory.CreateDirectory(newDirectoryPath);
+            }
+
+            // Determine the new file name and location based on the original file name
+            string fileName = Path.GetFileName(file);
+            string newFilePath = Path.Combine(newDirectoryPath, fileName);
+
+            // Move the file to the new location
+            File.Move(file, newFilePath);
+
+            REPEAT:
+                continue;
+        }
     }
 }
